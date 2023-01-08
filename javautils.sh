2f08aad0c947247a42794b8480a7967c109a055f
bhaskarmk <bhaskarmkul1098@gmail.com>
2023-01-09 00:19:04 +0530

    reverted..Revert "2023janupdates"
    This reverts commit fdc97793a5b8fe038be9559ff51eb8a0161cdb19.

--- javautils.sh
+++ javautils.sh
@@ -1,5 +1,4 @@
 echo "this is the script having the java iformation and java progrma"
 echo "hii"
 read -p "enter the program name that is there are not in the java file" proname
-echo "hii this are new lies"
------------------------
+
