1386487d3898cb21b67463d6c1498af77891ef1e
bhaskarmk <bhaskarmkul1098@gmail.com>
2023-01-09 00:45:47 +0530

    updating a

--- javautils.sh
+++ javautils.sh
@@ -1,4 +1,4 @@
 echo "this is the script having the java iformation and java progrma"
 echo "hii"
 read -p "enter the program name that is there are not in the java file" proname
-
+echo "this line updated in the development branch"
