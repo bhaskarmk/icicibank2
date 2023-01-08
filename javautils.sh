098ec6cea2354f06f0e0ffc54a8d00d02fd95207
bhaskarmk <bhaskarmkul1098@gmail.com>
2023-01-09 00:37:58 +0530

    newfiles

--- javautils.sh
+++ javautils.sh
@@ -1,4 +1,5 @@
 echo "this is the script having the java iformation and java progrma"
 echo "hii"
 read -p "enter the program name that is there are not in the java file" proname
+echo "hii this extra new lines"
 
